a9f2db752ae23b76105f8ae612a43e7d831a04e1
cszhjh <cszhjh@gmail.com>
2023-05-20 18:17:46 +0800

    http address

--- src/constants/base-url-config.js
+++ src/constants/base-url-config.js
@@ -1,6 +1,6 @@
 export { BASE_URL } from "@/service/request/config";
 
-const WEB_DOMAIN_BASE_URL = "http://111.230.110.195:8080";
+const WEB_DOMAIN_BASE_URL = "http://47.120.15.88";
 
 export const USER_BASE_URL = WEB_DOMAIN_BASE_URL + "/user/";
 
--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "^/api": {
-        target: "http://111.230.110.195:8000",
+        target: "http://47.120.15.88:8000",
         pathRewrite: {
           "^/api": ""
         },
